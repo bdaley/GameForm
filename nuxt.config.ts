9cd3e81948be3e6de774efdb939215dcbf441156
Brian Daley <brian@uconn.edu>
2023-03-15 11:24:43 -0400

    new domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
         auth: true,
         config: {
             apiKey: 'AIzaSyD8XNm6JuPn_JvB-mOkZYF1lntXl9JE0Ug',
-            authDomain: 'getgameform.firebaseapp.com',
+            authDomain: 'getgameform.com',
             projectId: 'getgameform',
             appId: '1:631880925853:web:443dea65019c7f380b4a74',
             measurementId: "G-JSJEG57E3C"
